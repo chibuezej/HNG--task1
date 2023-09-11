6f202b859e3ba70c719d0b4629d710133dc96856
chibuezej <johnhenrychibueze@gmail.com>
2023-09-11 20:08:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function displayUTCTimeMilliseconds() {
+  const utcTimeMillisecondsElement = document.querySelector(
+    '[data-testid="currentUTCTime"'
+  );
+
+  setInterval(() => {
+    const currentTimeMillis = new Date().getTime();
+    utcTimeMillisecondsElement.textContent = `UTC Time: ${currentTimeMillis}`;
+  }, 1000);
+}
+
+function displayDayOfWeek() {
+  const dayOfWeekElement = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+
+  setInterval(() => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDayOfWeek = new Date().getDay();
+    dayOfWeekElement.textContent = `Day of the Week: ${days[currentDayOfWeek]}`;
+  }, 1000);
+}
+
+displayUTCTimeMilliseconds();
+displayDayOfWeek();
